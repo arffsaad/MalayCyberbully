865aafc90cf2da1de846dca898259c775073b21b
Ariff Saad <80538339+arffsaad@users.noreply.github.com>
2022-04-09 14:08:25 +0800

    Add files via upload

---
+++ ui/tf.js
@@ -0,0 +1,28 @@
+function tfSock() {
+
+    if ("WebSocket" in window) {
+        var ws = new WebSocket("ws://localhost:9998/tensor");
+        console.log("WebSocket connected!");
+
+        ws.onopen = function() {
+            var x = document.getElementById("textInput").value
+            ws.send(x);
+            document.getElementById("alertText").textContent = x;
+            console.log("Message is sent...");
+        };
+        ws.onmessage = function(evt) {
+            var received_msg = evt.data;
+            console.log(evt.data)
+            if (evt.data == 'anger') {
+                $('#cybPrompt').modal()
+            }
+            ws.close()
+        };
+        ws.onclose = function() {
+            console.log("Connection is closed...");
+        };
+
+    } else {
+        alert("WebSocket NOT supported by your Browser!");
+    }
+}
