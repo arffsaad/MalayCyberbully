3d51f886e91cb979d2db55a70b64371a4762f6ec
Ariff Saad <80538339+arffsaad@users.noreply.github.com>
2022-05-23 23:32:16 +0800

    Add files via upload

---
+++ censor_wsh.py
@@ -0,0 +1,25 @@
+import six
+import tf # When testing is done, uncomment
+
+_GOODBYE_MESSAGE = u'Goodbye'
+
+def web_socket_do_extra_handshake(request): #ignore, assuming unneeded.
+    pass
+
+def web_socket_transfer_data(request): # change here.
+    while True:
+        line = request.ws_stream.receive_message()
+        if line is None:
+            return
+        if isinstance(line, six.text_type): # edit this function to include python calls
+            text = []
+            text.append(line)
+            final = str(tf.cen(text))
+            request.ws_stream.send_message(final, binary=False) # this line should return the result of the classification
+            if line == _GOODBYE_MESSAGE:
+                return
+        else:
+            request.ws_stream.send_message(line, binary=True)
+
+
+# vi:sts=4 sw=4 et
